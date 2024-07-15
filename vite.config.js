0c8ef82e4cda29d0e13d9daf1443888d9c35b11d
sututu <sututu666@hotmail.com>
2024-07-15 21:00:28 +0800

    测试

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8083,
+    strictPort:true,
+    watch:{
+      ignored:["**/src-tauri/**"],
+    }
+  },
   envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
   build: {
     // Tauri uses Chromium on Windows and WebKit on macOS and Linux
