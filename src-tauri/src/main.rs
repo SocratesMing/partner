03091bfcb7943caf2da3097d00b494b1b07e5ec6
sututu <sututu666@hotmail.com>
2024-07-30 18:59:40 +0800

    调试成功

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use tauri::Manager;
 
@@ -15,14 +16,62 @@ fn main() {
             }
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet,students])
+        .invoke_handler(tauri::generate_handler![greet,students,read_parquet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+fn greet(name: &str) -> (String, String) {
+    let x = format!("Hello, {}!", name);
+    let y = format!("bye, {}!", name);
+    (x, y)
+}
+
+
+#[tauri::command]
+fn read_parquet(path: &str, num: i32) -> (String, String) {
+    let mut file = std::fs::File::open(path).unwrap();
+    let df = ParquetReader::new(&mut file).finish().unwrap();
+    let df_d = &df;
+
+    let mut title = df_d.get_column_names();
+    let (rows, columns) = df_d.shape();
+    println!("{:?}", title);
+    println!("{} {}", rows, columns);
+    let frame = df_d.head(Some(10));
+    title.push("key");
+
+    let mut res: Vec<HashMap<String, String>> = Vec::new();
+    for i in 0..10 {
+        let mut map = HashMap::new();
+        map.insert(format!("key"), i.to_string());
+        let result = frame.get_row(i).unwrap();
+        for (i, v) in result.0.iter().enumerate() {
+            map.insert(title.get(i).unwrap().to_string(), v.to_string());
+            println!("{:?}", v);
+        }
+        res.push(map);
+    }
+    let value = serde_json::to_string(&res).unwrap();
+
+    let mut titles: Vec<HashMap<String, String>> = Vec::new();
+    for t in title.iter() {
+        if t.to_string() == "key" {
+            continue;
+        }
+        let mut map = HashMap::new();
+
+        map.insert("title".to_string(), t.to_string());
+        map.insert("key".to_string(), t.to_string());
+        map.insert("dataIndex".to_string(), t.to_string());
+
+        titles.push(map);
+    }
+    let ttt = serde_json::to_string(&titles).unwrap();
+    println!("{}", value);
+    println!("{}", ttt);
+    (ttt, value)
 }
 
 
@@ -62,17 +111,10 @@ fn test() {
 }
 use polars::prelude::*;
 #[test]
-fn test2(){
+#[warn(unused_imports)]
+#[warn(unused_variables)]
+fn test2() {
     let path = r"D:\Code\Python\fast-backtest\back_test_local\data\HDATA_HO\EURUSDSP\TICK_BAR_1H\201801.parquet";
 
-    // let mut file = std::fs::File::open(r"D:\Code\Python\fast-backtest\back_test_local\data\HDATA_HO\EURUSDSP\TICK_BAR_1H\201801.parquet").unwrap();
-
-
-    // let df = ParquetReader::new(&mut file).finish().unwrap();
-    let args = ScanArgsParquet::default();
-    let lf = LazyFrame::scan_parquet(path, args).unwrap();
-    for row in lf{
-        println!("{}", row);
-
-    }
+    read_parquet(path, 10);
 }
--- src/component/navicate.jsx
+++ src/component/navicate.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { invoke } from '@tauri-apps/api'
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { Flex, Menu, Button } from 'antd';
 import ShowData from './datashow';
@@ -7,12 +8,7 @@ import { Space, Table, Tag } from 'antd';
 let initialNavi = [{
   label: 'csv file',
   key: 'mail',
-  children: [
-    {
-      key: 12,
-      label: 'Option 13',
-    },
-  ]
+  children: []
 },
 {
   label: 'parquet file',
@@ -20,7 +16,53 @@ let initialNavi = [{
   children: []
 },]
 
-
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+  },
+];
 const initialTabs = [
   // {
   //   label: 'Tab 1',
@@ -32,8 +74,12 @@ const Navi = () => {
 
   const [current, setCurrent] = useState(initialNavi);
   const [tabs, setTabs] = useState(initialTabs);
-  const [key, setKey] = useState(1);
   const [used, setUsed] = useState([]);
+  const [key, setKey] = useState(1);
+  const [titles, setTitles] = useState(columns);
+  const [tables, setTables] = useState(data);
+  const [path, setPath] = useState("");
+
 
   // 打开文件读取对话框
   async function selectFile() {
@@ -80,8 +126,12 @@ const Navi = () => {
                 key: key,
                 label: file,
               },]
+              readParquetFile(selected);
+              // test2();
             }
           }
+
+
           return e;
         }))
 
@@ -94,54 +144,46 @@ const Navi = () => {
     }
   }
 
+  function readParquetFile(file_path, rows = 20) {
+    invoke('read_parquet', { path: file_path, num: rows })
+      // `invoke` 返回的是一个 Promise
+      .then((response) => {
+        console.log("======");
+        console.log(response[0]);
+        console.log(response[1]);
+        console.log("======");
+        let rr1 = JSON.parse(response[0]);
+        let rr2 = JSON.parse(response[1]);
+        console.log(rr1);
+        console.log(rr2);
+        setTitles(rr1);
+        setTables(rr2);
+        console.log("返回了数据 readParquetFile");
+      })
+  }
 
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-    },
-    {
-      title: 'Action',
-      key: 'action',
-    },
-  ];
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sydney No. 1 Lake Park',
-    },
-  ];
+  function test() {
+    invoke('greet', { name: 'World' })
+      // `invoke` 返回的是一个 Promise
+      .then((rr1) => {
+        console.log("----");
+        console.log(r1[0]);
+        console.log(r1[1]);
+        console.log("返回了数据 test");
+      })
+  }
+
+  function test2() {
+    let hello = invoke('students')
+      // `invoke` 返回的是一个 Promise
+      .then((response) => {
+        console.log(response);
+        let ddd = JSON.parse(response)
+        console.log("返回了数据 test2");
+        console.log(ddd);
+      })
+  }
+
 
 
   return (
@@ -157,15 +199,18 @@ const Navi = () => {
           // onOpenChange={onOpenChange}
           style={{
             width: "20%",
-            height: 512
+            height: 666
+
           }}
           items={current}
         />
-        <Table columns={columns} dataSource={data} style={{
-            width: "80%",
-            height: 512
-          }} />
-      </Flex>
+
+        <Table columns={titles} dataSource={tables} style={{
+          width: "80%",
+          height: "100%"
+        }} size="small" scroll={{ x: 'max-content' }}
+          pagination={{ defaultCurrent: 1, total: 2 }} />
+      </Flex >
 
     </>
 
